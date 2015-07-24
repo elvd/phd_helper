a70c78400ac4b4ff095fc6e200a4472816138352
elvd <elvd@leeds.ac.uk>
2015-07-24 17:37:31 +0100

    Final xml tweak. Added graph creation and saving.

--- sim_data_graph.py
+++ sim_data_graph.py
@@ -7,6 +7,7 @@ Created on Thu Jul 23 15:38:36 2015
 
 import os
 import lxml.etree as etree
+import matplotlib.pyplot as plt
 
 
 def construct_legend(legend_xml):
@@ -20,6 +21,8 @@ filename_list = os.listdir(os.getcwd())
 filename_list = [filename for filename in filename_list if
                  os.path.splitext(filename)[1] == '.xml']
 
+graph_counter = 0
+
 for filename in filename_list:
     graph_info = etree.parse(filename)
     graph_info_root = graph_info.getroot()
@@ -29,25 +32,44 @@ for filename in filename_list:
         graph_labels = graph.find('labels')
 
         if subgraphs_separate == 'yes':
-            for subgraph in graph.find('subgraphs'):
-                for data_key in graph.find('datasets'):
-                    print data_key.text
-                    print int(subgraph.text)
-                print graph_labels.find('xlabel').text
-                print graph_labels.find('ylabel').text
-                subgraph_title = ''.join([".//*[@subgraph='",
-                                         subgraph.text, "']"])
-                print graph_labels.find(subgraph_title).text
-                print construct_legend(graph_labels.find('legend'))
+            outer = 'subgraphs'
+            inner = 'datasets'
         else:
-            for data_key in graph.find('datasets'):
-                for subgraph in graph.find('subgraphs'):
-                    print data_key.text
-                    print int(subgraph.text)
-                print graph_labels.find('xlabel').text
-                print graph_labels.find('ylabel').text
-                print graph_labels.find('title').text
-                print construct_legend(graph_labels.find('legend'))
+            outer = 'datasets'
+            inner = 'subgraphs'
 
-        print 'next graph'
-    print 'next file'
+        for subgraph in graph.find(outer):
+            fig = plt.figure()
+            ax1 = fig.add_subplot(111)
+            ax1.set_color_cycle(['r', 'k', 'b', 'g', 'c', 'm'])
+
+            for data_key in graph.find(inner):
+                plot_entry = sim_results[data_key.text][int(subgraph.text)]
+                ax1.plot(plot_entry[:, 0], plot_entry[:, 1], lw=1.0)
+
+            ax1.set_xlabel(graph_labels.find('xlabel').text)
+            ax1.set_ylabel(graph_labels.find('ylabel').text)
+            subgraph_title = ''.join([".//*[@subgraph='",
+                                     subgraph.text, "']"])
+            ax1.set_title(graph_labels.find(subgraph_title).text)
+            ax1.legend(construct_legend(graph_labels.find('legend')))
+
+            ax1.set_axisbelow(True)
+            ax1.grid(which='both', axis='both', color='0.1', ls=':',
+                     lw=0.2)
+            ax1.tick_params(direction='out', top='off', right='off',
+                            width=0.5)
+
+            for spine in ['left', 'top', 'right', 'bottom']:
+                ax1.spines[spine].set_linewidth(0.5)
+
+            plt.rc('font', family='serif')
+            plt.rc('legend', fontsize=12)
+            plt.rc('axes', titlesize=14)
+            plt.rc('axes', labelsize=12)
+            plt.rc('xtick', labelsize=12)
+            plt.rc('ytick', labelsize=12)
+
+            graph_filename = '.'.join([str(graph_counter), 'jpg'])
+            plt.savefig(graph_filename, dpi=300)
+            graph_counter += 1
